2005afb73a10584a24b8322f542d42cd401d1711
maxi <maximilian.lincks@gmx.net>
2024-08-18 14:08:58 +0200

    init

--- src/statistic/core.clj
+++ src/statistic/core.clj
@@ -1,8 +1,36 @@
 (ns statistic.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]))
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn fps-handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Pew Pew!"})
+
+(defn mail-man []
+  "{\"Spongebob Narrator\": \"5 years later...\"}")
+
+(defn mail-handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (mail-man)})
+
+(defn general-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "All hail General Zod!"})
+
+(defroutes app-routes
+           (GET "/" [] fps-handler)
+           (GET "/postoffice" [] mail-handler)
+           (ANY "/anything-goes" [] general-handler)
+           (route/not-found "You must be new here"))
+
+(defn -main
+  "This is the apps entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
