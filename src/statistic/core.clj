697927e0a62c539595d5e5ba73f2d65319fa42e1
Maxi <maximilian.lincks@gmx.net>
2024-08-22 23:21:17 +0200

    added initial index.html and dummy handler

--- src/statistic/core.clj
+++ src/statistic/core.clj
@@ -4,21 +4,32 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [org.httpkit.server :as server]
-            [statistic.db.tables.players :as players]))
+            [statistic.db.tables.players :as players]
+            [next.jdbc.date-time :as date-time]))
 
 (defn fps-handler [req]
   {:status  200
    :headers {"Content-Type" "text/json"}
-   :body    (json/write-str (players/get-players))})
+   :body    (json/write-str (players/get-all))})
+
+;;TODO figure out how to handle request parameters
+;;TODO figure out how to create sessions (hint: ring?)
+(defn login-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "hallo"})
 
 (defroutes app-routes
            (GET "/" [] fps-handler)
            (GET "/players" [] fps-handler)
+           (POST "/login" [] login-handler)
            (route/not-found "Error 404 - route not found"))
 
 (defn -main
   "This is the apps entry point"
   [& args]
+  ;; move to setup function in separate namespace
+  (date-time/read-as-local)
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
     (server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
