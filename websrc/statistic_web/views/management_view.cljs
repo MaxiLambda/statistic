3cd06a099d7c9c9be8d5475764cf4f77e1e636d0
Maxi <maximilian.lincks@gmx.net>
2024-09-17 00:04:28 +0200

    management view is a bit prettier now

--- websrc/statistic_web/views/management_view.cljs
+++ websrc/statistic_web/views/management_view.cljs
@@ -2,6 +2,7 @@
   (:require [re-frame.core :as re-frame]
             [reagent-mui.material.autocomplete :refer [autocomplete]]
             [reagent-mui.material.form-control :refer [form-control]]
+            [reagent-mui.material.grid :refer [grid]]
             [reagent-mui.material.input-label :refer [input-label]]
             [reagent-mui.material.menu-item :refer [menu-item]]
             [reagent-mui.material.select :refer [select]]
@@ -47,85 +48,96 @@
   (let [players @(re-frame/subscribe [::subs/players])
         tags @(re-frame/subscribe [::subs/tags])
         disciplines @(re-frame/subscribe [::subs/disciplines])]
-    [form-control
+    [grid {:columns 1 :md true :style {:border "1px solid black" :padding "1%"}}
      [:h3 "Create a new Match"]
-     [date-time-picker {:label          "Date and Time of the Match"
-                        :format         "dd/MM/yyyy - HH:mm"
-                        :ampm           false
-                        :value          (:date @match-form)
-                        :on-change      #(swap! match-form assoc :date %)
-                        :view-renderers {:hours   renderTimeViewClock
-                                         :minutes renderTimeViewClock}
-                        }]
-     [:br]
-     [form-control                                          ;;if select is not wrapped in its own form-control the label goes crazy
-      [input-label {:id "winner-label"
-                    :sx {:bgcolor "background.default"}}
-       "Who won?"
+     [form-control {:fullWidth true}
+      [date-time-picker {:label          "Date and Time of the Match"
+                         :format         "dd/MM/yyyy - HH:mm"
+                         :ampm           false
+                         :value          (:date @match-form)
+                         :on-change      #(swap! match-form assoc :date %)
+                         :view-renderers {:hours   renderTimeViewClock
+                                          :minutes renderTimeViewClock}
+                         }]
+      [:br]
+      [form-control                                         ;;if select is not wrapped in its own form-control the label goes crazy
+       [input-label {:id "winner-label"
+                     :sx {:bgcolor "background.default"}}
+        "Who won?"
+        ]
+       [select {:labelId   "winner-label"
+                :value     (:winner @match-form)
+                :on-change #(->> % .-target .-value (swap! match-form assoc :winner))
+                :variant   "outlined"}
+        [menu-item {:value 0 :key 0} "Draw"]
+        [menu-item {:value 1 :key 1} "Team 1"]
+        [menu-item {:value 2 :key 2} "Team 2"]
+        ]]
+      [:br]
+      [grid {:columns 2 :display "flex"}
+       [autocomplete {:fullWidth   true
+                      :sx          {:pr 1}
+                      :options     (or tags [])
+                      :freeSolo    true                     ;;enables custom values
+                      :multiple    true
+                      :on-change   #(->> %2 js->clj (swap! match-form assoc :tags))
+                      ;;https://stackoverflow.com/questions/63944323/problem-with-autocomplete-material-ui-react-reagent-clojurescript
+                      :renderInput #(do
+                                      (set! (.-label %) "Match Tags")
+                                      (r/create-element mui/TextField %))}
+        ]
+       [autocomplete {:fullWidth       true
+                      :sx              {:pl 1}
+                      :options         (or disciplines [])
+                      :freeSolo        true                 ;;enables custom values
+                      :value           (or (:discipline match-form) "")
+                      :on-input-change #(->> %2 js->clj (swap! match-form assoc :discipline)) ;;on-change only fires on custom values...
+                      ;;https://stackoverflow.com/questions/63944323/problem-with-autocomplete-material-ui-react-reagent-clojurescript
+                      :renderInput     #(do
+                                          (set! (.-label %) "Match Discipline")
+                                          (r/create-element mui/TextField %))}
+        ]
        ]
-      [select {:labelId   "winner-label"
-               :value     (:winner @match-form)
-               :on-change #(->> % .-target .-value (swap! match-form assoc :winner))
-               :variant   "outlined"}
-       [menu-item {:value 0 :key 0} "Draw"]
-       [menu-item {:value 1 :key 1} "Team 1"]
-       [menu-item {:value 2 :key 2} "Team 2"]
-       ]]
-     [:br]
-     [autocomplete {:options     (or tags [])
-                    :freeSolo    true                       ;;enables custom values
-                    :multiple    true
-                    :on-change   #(->> %2 js->clj (swap! match-form assoc :tags))
-                    ;;https://stackoverflow.com/questions/63944323/problem-with-autocomplete-material-ui-react-reagent-clojurescript
-                    :renderInput #(do
-                                    (set! (.-label %) "Match Tags")
-                                    (r/create-element mui/TextField %))}
-      ]
-     [:br]
-     [autocomplete {:options         (or disciplines [])
-                    :freeSolo        true                   ;;enables custom values
-                    :value           (or (:discipline match-form) (first disciplines) "")
-                    :on-input-change #(->> %2 js->clj (swap! match-form assoc :discipline)) ;;on-change only fires on custom values...
-                    ;;https://stackoverflow.com/questions/63944323/problem-with-autocomplete-material-ui-react-reagent-clojurescript
-                    :renderInput     #(do
-                                        (set! (.-label %) "Match Discipline")
-                                        (r/create-element mui/TextField %))}
-      ]
-     [:br]
-     [form-control                                          ;;if select is not wrapped in its own form-control the label goes crazy
-      [input-label {:id "team1-label"
-                    :sx {:bgcolor "background.default"}}
-       "Team 1"
-       ]
-      [select {:labelId   "team1-label"
-               :multiple  true
-               :value     (:team1 @match-form)
-               :on-change #(->> % .-target .-value vec (update-players :team1 :team2))
-               :variant   "outlined"}
-       (for [player players]
-         [menu-item {:value (:id player) :key (:id player)} (:name player)])
-       ]
-      ]
-     [:br]
-     [form-control                                          ;;if select is not wrapped in its own form-control the label goes crazy
-      [input-label {:id "team2-label"
-                    :sx {:bgcolor "background.default"}}
-       "Team 2"
-       ]
-      [select {:labelId   "team2-label"
-               :multiple  true
-               :value     (:team2 @match-form)
-               :on-change #(->> % .-target .-value vec (update-players :team2 :team1))
-               :variant   "outlined"}
-       (for [player (remove (set (:team1 @match-form)) players)]
-         [menu-item {:value (:id player) :key (:id player)} (:name player)])
+      [:br]
+      [grid {:columns 2 :display "flex"}
+       [form-control {:fullWidth true
+                      :sx        {:pr 1}}                   ;;if select is not wrapped in its own form-control the label goes crazy
+        [input-label {:id "team1-label"
+                      :sx {:bgcolor "background.default"}}
+         "Team 1"
+         ]
+        [select {:labelId   "team1-label"
+                 :multiple  true
+                 :value     (:team1 @match-form)
+                 :on-change #(->> % .-target .-value vec (update-players :team1 :team2))
+                 :variant   "outlined"}
+         (for [player players]
+           [menu-item {:value (:id player) :key (:id player)} (:name player)])
+         ]
+        ]
+       [:br]
+       [form-control {:fullWidth true
+                      :sx        {:pl 1}}                   ;;if select is not wrapped in its own form-control the label goes crazy
+        [input-label {:id "team2-label"
+                      :sx {:bgcolor "background.default"}}
+         "Team 2"
+         ]
+        [select {:labelId   "team2-label"
+                 :multiple  true
+                 :value     (:team2 @match-form)
+                 :on-change #(->> % .-target .-value vec (update-players :team2 :team1))
+                 :variant   "outlined"}
+         (for [player (remove (set (:team1 @match-form)) players)]
+           [menu-item {:value (:id player) :key (:id player)} (:name player)])
+         ]
+        ]
        ]
       ]
      ]
     ))
 
 
-;;to create a new player
+;;TODO to create a new player form
 ;;name -> char(20)
 
 
