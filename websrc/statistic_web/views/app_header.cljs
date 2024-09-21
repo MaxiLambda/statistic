d1aa97ec4b7efd39b7fc39d2965771ea5eb1ad12
Maxi <maximilian.lincks@gmx.net>
2024-09-21 15:43:29 +0200

    make app bar indicate current view

--- websrc/statistic_web/views/app_header.cljs
+++ websrc/statistic_web/views/app_header.cljs
@@ -5,40 +5,38 @@
             [reagent-mui.material.button :refer [button]]
             [reagent-mui.material.toolbar :refer [toolbar]]
             [reagent-mui.material.typography :refer [typography]]
-            [statistic-web.re-frame.global-events :as events]))
+            [statistic-web.re-frame.global-events :as events]
+            [statistic-web.re-frame.global-subs :as subs]))
+
+(defn switch-view-button
+  [view-key button-text current_view]
+  [button
+   {:variant  "contained"
+    :color    (if (= current_view view-key) "secondary" "info")
+    :on-click #(re-frame/dispatch [::events/path-change view-key])}
+   button-text
+   ])
 
 (defn header [children]
-  [:div
-   [box
-    {:sx {:flex-grow 1}}
-    [app-bar
-     {:position "static"}
-     [toolbar
-      [typography
-       {:component "div"
-        :variant   "h6"
-        :sx        {:flex-grow 1}}
-       "Statistics"
-       ]
-      [button
-       {:variant  "contained"
-        :color    "secondary"
-        :on-click #(re-frame/dispatch [::events/path-change :leaderboard])}
-       "Leaderboard"
-       ]
-      [button
-       {:variant "contained"
-        :color "secondary"
-        :on-click #(re-frame/dispatch [::events/path-change :archive])}
-       "Archive"]
-      [button
-       {:variant  "contained"
-        :color    "primary"
-        :on-click #(re-frame/dispatch [::events/path-change :management])}
-       "Edit (Admin)"
+  (let [view (:name @(re-frame/subscribe [::subs/view]))]
+    [:div
+     [box
+      {:sx {:flex-grow 1}}
+      [app-bar
+       {:position "static"}
+       [toolbar {:sx {:gap "10px"}}
+        [typography
+         {:component "div"
+          :variant   "h6"
+          :sx        {:flex-grow 1}}
+         "Statistics"
+         ]
+        (switch-view-button :leaderboard "Leaderboard" view)
+        (switch-view-button :archive "Archive" view)
+        (switch-view-button :management "Create Match" view)
+        ]
        ]
       ]
-     ]
-    ]
-   children
-   ])
+     children
+     ]))
+
