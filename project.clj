4682246b0fbeecc7db8e148777ceada1dd4ab20f
Maxi <maximilian.lincks@gmx.net>
2024-09-30 13:17:56 +0200

    update from clojure 1.11.1 to 1.12.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [http-kit/http-kit "2.8.0"]
                  [compojure "1.7.1"]
                  [com.github.seancorfield/next.jdbc "1.3.834"]
